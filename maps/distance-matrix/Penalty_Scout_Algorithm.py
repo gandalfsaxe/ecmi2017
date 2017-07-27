ac3fe4de2b651f57ff8863327abbefb6f490f87b
carlassmann <carlassmann@me.com>
2017-07-27 18:14:25 +0200

    penalty scout code updated
    penalty scout algorithm is now shorter and has more comments to ease up the use

--- maps/distance-matrix/Penalty_Scout_Algorithm.py
+++ maps/distance-matrix/Penalty_Scout_Algorithm.py
@@ -1,24 +1,13 @@
 # python 2
-# idea here will be to compute efficient but half random paths to find an efficient one
+# use smart heuristics to compute shortest path
 
 import networkx as nx
 import random
-import os
 import numpy as np
 import csv
 import time
-import matplotlib as mp
 
-"""
-G = nx.Graph()
 
-edges = [('A', 'B', 6), ('A', 'C', 6), ('A', 'D', 7), ('B', 'D', 5), ('B', 'E', 10), ('C', 'D', 8), ('C', 'F', 7),
-         ('D', 'E', 6), ('D', 'G', 9), ('D', 'F', 11), ('E', 'G', 8), ('E', 'H', 7), ('F', 'G', 10), ('F', 'H', 9),
-         ('G', 'H', 5)]
-
-for start, end, length in edges:
-    G.add_edge(start, end, attr_dict={'length' : length, 'visits' : 0})
-"""
 def create_graph_from_distance_matrix(distance_matrix):
     ''' Creates graph from a matrix in which the element i,j indicates the distance between i and j '''
     node_num = distance_matrix.shape[0]
@@ -34,44 +23,52 @@ def create_graph_from_distance_matrix(distance_matrix):
 
 
 def manual_map_one():
-    ''' Creates the manual map '''
+    ''' Creates the manual map using a CSV file containing the distance matrix. '''
+
+    # Change the path to get the right file with your distance matrix!
     path = "/Users/Carl/Documents/GitHub/ecmi2017/data/Distance matrix for manual graph/"
     filename = 'DistanceMatrix.csv'
+
     distance_matrix = np.loadtxt(open(path+filename, "rb"), delimiter=",", skiprows=0)
     graph = create_graph_from_distance_matrix(distance_matrix)
+
     return graph
 
 
 def valuevertex(A, Walk, graph, penalty):
-    """return value of vertex"""
-    quality_A = 0
+    """Returns the penaltyvalue of the vertex. High penalty -> low possibilty to be choosen as next point"""
+
+    quality_a = 0
 
     # first check if A or B cause a U-turn
     if len(Walk) > 1:
         if A == Walk[-2]:
-            quality_A += 10
+            quality_a += 10
 
     # length of the path
     now = Walk[-1]
-    quality_A += G[now][A]['length']# * graph[now][A]['visits']
+    quality_a += G[now][A]['length']# * graph[now][A]['visits']
 
     # number of visits of the path
-    quality_A += 2 * penalty * graph[now][A]['visits']
+    quality_a += 2 * penalty * graph[now][A]['visits']
 
-    return quality_A
+    return quality_a
 
 
 def nextpoint(walk, graph, penalty):
         """this function delivers a decision for the next point to go, looking at the graph and the walk"""
         nodenow = walk[-1]
+
         points = graph.neighbors(nodenow)
-        # sort the points to have best vertex first
+
+        # sort the points to have best vertex (with the lowest penalty) first
         if len(points)>1:
             i = 0
             # connect value to every point
             while i < len(points):
                 points[i] = [points[i], valuevertex(points[i], walk, graph, penalty)]
                 i += 1
+
             # sort the list of points by the value connected
             points.sort(key=lambda x: x[1])
 
@@ -81,6 +78,8 @@ def nextpoint(walk, graph, penalty):
         # now decide which point should be next, with highest chance for best point and lowest chance for worst point
         borders = range(1, len(points) + 1)[::-1] # e.g. for 3 points gives [3,2,1]
 
+        # here the first value gets increased, which results in a higher possibility for the first point
+        # we found that this change improves the quality of generated walks
         borders[0] *= 10
 
         # generate a random number to decide which point to go to
@@ -107,69 +106,67 @@ def my_endwhile(walk, list):
         else: return False
 
 
-def generate_walk(start, graph, expectedfitness, machines = 1, optimize='length'):
+def generate_walk(start, graph, expectedfitness):
 
     """this function generates a walk through the graph with given start point
     start point is also ending point
 
     output is the generated walk and the fitness as a list"""
     g = graph.copy()
-    walk = [start] * machines
-    finished = [False] * machines
+    walk = start[:]
+
+    finished = [0]
+
+    # fitness is the length of the path, a lower fitness is better
     fitness = 0
+
     listofedges = graph.edges()
     fraud_candidate = True
     final_walk = []
     # repeat the iteration until the graph is fully covered and current position is starting position
     while len(finished) != 0:
 
-        for i in range(len(walk)):
-            walk_machine = walk[i][:]
-            now = walk_machine[-1]
-            next_vert = nextpoint(walk_machine, g, len(listofedges))
-            walk_machine.append(next_vert)
-            try:
-                g.edge[now][next_vert]['visits'] += 1
-                if optimize == 'length':
-                    fitness += g.edge[now][next_vert]['length']
-                elif optimize == 'time':
-                    if g.edge[now][next_vert]['visits'] == 1:
-                        fitness += (g.edge[now][next_vert]['length']/15)*60
-                    else:
-                        fitness += (g.edge[now][next_vert]['length']/35)*60
-            except:
-                g.edge[next_vert][now]['visits'] += 1
-                if optimize == 'length':
-                    fitness += g.edge[now][next_vert]['length']
-                elif optimize == 'time':
-                    if g.edge[now][next_vert]['visits'] == 1:
-                        fitness += (g.edge[now][next_vert]['length']/15)*60
-                    else:
-                        fitness += (g.edge[now][next_vert]['length']/35)*60
-            try:
-                listofedges.remove((next_vert, now))
-            except:
-                fitness += 0
-            try:
-                listofedges.remove((now, next_vert))
-            except:
-                fitness += 0
-            walk[i] = walk_machine
-            if my_endwhile(walk_machine, listofedges):
-                final_walk.append(walk_machine)
-                walk.remove(walk[i])
-                finished = finished[1:]
-                break
+        # here we compute our next step
+        now = walk[-1]
+        # where to go next
+        next_vert = nextpoint(walk, g, len(listofedges))
+        # append next point to the walk
+        walk.append(next_vert)
+
+        # increase number of visits for choosen edge and increase fitness
+        try:
+            g.edge[now][next_vert]['visits'] += 1
+            fitness += g.edge[now][next_vert]['length']
+        except:
+            g.edge[next_vert][now]['visits'] += 1
+            fitness += g.edge[now][next_vert]['length']
+
+        try:
+            listofedges.remove((next_vert, now))
+        except:
+            fitness += 0
+        try:
+            listofedges.remove((now, next_vert))
+        except:
+            fitness += 0
+
+        # if fitness is higher than expected fitness, there is no need to continue walking
         if fitness > expectedfitness:
-            # we get here only if the fitness of current walk is worse than the expected one
-            # the walk is not going to be saved
             fraud_candidate = False
             break
 
-    return [final_walk, fitness, machines, fraud_candidate, optimize]
+        # Here we decide, if the while loops needs to end using the my_endwhile function
+        if my_endwhile(walk, listofedges):
+            final_walk = walk
+
+            finished = []
+
+            break
+
+    return [final_walk, fitness, fraud_candidate]
 
 
-def start_penalty_scout(start, graph, N, machinenumber, optimize='length'):
+def start_penalty_scout(start, graph, N):
 
     G = graph.copy()
     # first generate initial guess of length,
@@ -178,125 +175,50 @@ def start_penalty_scout(start, graph, N, machinenumber, optimize='length'):
     for i in temp:
         totallength += i[2]['length']
 
-    # fitness in the end should be lower than the following value
-    expectedfitness = totallength * 2 * machinenumber
-    expectedfitness = expectedfitness * 4 if optimize=='time' else expectedfitness * 1
+    # we expect the optimum route to be lower than 2 times the total length of the path, this increases computing time
+    expectedfitness = totallength * 2
 
     W = start[:]
 
-    candidate_old = generate_walk(W, G, expectedfitness, machinenumber, optimize)
+    candidate_old = generate_walk(W, G, expectedfitness)
     iteration = 0
 
-    table = [['iteration', 'fitness', 'machines running', 'fitness', 'walkingpath']]
+    table = [['iteration', 'fitness', 'walkingpath']]
 
     for i in range(N):
 
-        candidate_new = generate_walk(W, G, expectedfitness, machinenumber, optimize)
+        candidate_new = generate_walk(W, G, expectedfitness)
         if candidate_new[1] < candidate_old[1]:
             candidate_old = candidate_new
             expectedfitness = candidate_old[1]
             iteration = i
-            if candidate_old[3]:
-                table.append([iteration, expectedfitness, machinenumber, optimize, candidate_old[0]])
-    if optimize == 'length':
-        with open('penaltyscoutloglength.csv', 'w') as csvfile:
-            writer = csv.writer(csvfile)
-            [writer.writerow(r) for r in table]
-    elif optimize == 'time':
-        with open('penaltyscoutlogltime.csv', 'w') as csvfile:
-            writer = csv.writer(csvfile)
-            [writer.writerow(r) for r in table]
+            if candidate_old[2]:
+                table.append([iteration, expectedfitness])
 
+    with open('penaltyscoutloglength.csv', 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        [writer.writerow(r) for r in table]
 
     return [iteration, candidate_old]
 
 
 if __name__ == '__main__':
-    """
+
+    # generate graph
     G = manual_map_one()
-    
-"""
-    G = nx.complete_graph(50)
-    nx.set_edge_attributes(G, 'length', 1)  # random.uniform(0, 1))
-    nx.set_edge_attributes(G, 'visits', 0)
+    # Choose start and end point
     W = [0]
-
-    temp = G.edges(None, True)
-    totallength = 0
-
-    for i in temp:
-        totallength += i[2]['length']
-
-    print(totallength)
-
+    # take the time
     tt = time.time()
-
-    machinenumber = 1
+    # set the number of iterations
     N = 300
+    # calculate the candidate
+    candidate = start_penalty_scout(W, G, N)
 
-    """Calculating optimum for shortest time"""
-
-    """
-    
-    valueofinterest = 'time'
-
-    candidate = start_penalty_scout(W, G, N, machinenumber, valueofinterest)
-    iteration = candidate[0]
-
-    print("\n" + 'END OF PROCESS, possible optimum is:')
-    print(str(candidate[1][0]) + "\n" + str(candidate[1][1]) + "\n")
-    print('Optimizing parameter was:')
-    print(valueofinterest)
-    print('at iteration number:')
-    print(iteration)
-    """
-
-    """Calculating optimum for shortest length"""
-
-    valueofinterest = 'length'
-
-    candidate = start_penalty_scout(W, G, N, machinenumber, valueofinterest)
-    iteration = candidate[0]
-
-    print("\n" + 'END OF PROCESS, possible optimum is:')
-    print(str(candidate[1][0]) + "\n" + str(candidate[1][1]) + "\n")
-    print('Optimizing parameter was:')
-    print(valueofinterest)
-    print('at iteration number:')
-    print(iteration)
-
-    print 'Ellapsed time: %.4f seconds' % (time.time() - tt)
-
-
-"""
-N = 10
-
-timetime = []
-edgesnumber =[]
-for i in range(N-1, N):
-    G = nx.complete_graph(50*i)
-    nx.set_edge_attributes(G, 'length', 1) #random.uniform(0, 1))
-    nx.set_edge_attributes(G, 'visits', 0)
-    times = []
-    for j in range(3):
-
-        tt = time.time()
-        generate_walk([0], G, 100*i*i, 1)
-        times.append((time.time() - tt))
-        if j == 5 and i == 5:
-            nx.draw(G)
-            print(generate_walk([0], G, 100*j*j, 1))
-            print('this actually happened')
-    timetime.append(sum(times)/len(times))
-    edgesnumber.append(len(G.edges()))
-
-print(range(3, N))
-print(timetime)
-print(edgesnumber)
-
-"""
-
-
-
-
-
+    print('Possible optimum found with length:')
+    print(candidate[1])
+    print('After')
+    print(candidate[0])
+    print('iterations')
+    print('time needed:')
+    print(str(time.time()-tt))
